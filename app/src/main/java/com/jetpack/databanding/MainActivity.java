2544a9a2ac3fac808a5c06e0e0e1ab9f028c37a3
PPadhy <priyabrat.padhy@harman.com>
2019-01-30 12:19:12 +0530

    Initial commit

---
+++ app/src/main/java/com/jetpack/databanding/MainActivity.java
@@ -0,0 +1,52 @@
+package com.jetpack.databanding;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.jetpack.databanding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setStudent(getStudent());
+
+        StudentClickHandler studentClickHandler = new StudentClickHandler(getApplicationContext());
+        activityMainBinding.setClickHandler(studentClickHandler);
+
+    }
+
+    public Student getStudent(){
+        Student student = new Student();
+        student.setId(0);
+        student.setName("Golu");
+        return student;
+    }
+
+    public class StudentClickHandler {
+
+        private Context context;
+
+        public StudentClickHandler(Context context) {
+            this.context = context;
+        }
+
+
+        public void getIdClicked(View view){
+            Toast.makeText(context, "getId Clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        public void getNameClicked(View view){
+            Toast.makeText(context, "getName Clicked", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
